2061f7d6caaf658bcdc2815c874eee850e42c40e
Adam <a.chodkowski44412@gmail.com>
2020-11-02 12:18:46 +0100

    test
    test

---
+++ bot.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const broadcast = client.voice.createBroadcast();
+broadcast.play('pogrzeb.mp3');
+
+
+
+client.on('message', async message => {
+    if (!message.guild) return;
+  
+    if (message.content === '/pogrzeb') {
+      if (message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+        for (const connection of client.voice.connections.values()) {
+            connection.play(broadcast);
+        }
+      } else {
+        message.reply('Nie ma cie na voice chacie');
+      }
+    }
+  });
+
+
+client.login('YOUR TOKEN'); // your token
