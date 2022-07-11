caecbb9b5a9e67bfa453916f2284de699e10ca8d
Adam <53305070+Wepted@users.noreply.github.com>
2022-07-11 23:22:41 +0200

    trying to make discord v13 working

--- bot.js
+++ bot.js
@@ -1,57 +1,49 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const config = require("./config.json");
 
 
 
-client.on('ready' , () =>{
- console.log('ready!');
- client.user.setStatus('dnd');
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setStatus("dnd");
+});
+client.on('messageCreate', message => {
+
+
+  
+
+
+
+if (message.content === `${config.prefix}stop`) {
+
+  for (const connection of client.voice.connections.values()) {
+    connection.disconnect();
+}
+  console.log(`pogrzeb sie zakonczyl`);     
+  }
+
+
+
+if (message.content === `${config.prefix}pomoc`) {
+
+  message.channel.send("/pogrzeb - odprawia pogrzeb");
+  message.channel.send("/stop - konczy pogrzeb");
+  message.channel.send("/pingu - ping sprawdza w ms");
+  message.channel.send("https://cdn.discordapp.com/avatars/790380203296948224/96b25cdbb57b54fb52befca60fe3edfb.webp?size=1024");
+
+
+console.log(`ktos pomocy szuka`);  
+
+}
+
+
+  if (message.content === `${config.prefix}ping`) {
+    var ping = Date.now() - message.createdTimestamp + " ms";
+    message.channel.send("ping:`" + `${Date.now() - message.createdTimestamp}` + " ms`");
+    message.channel.send('pong');
+  }
 });
 
-client.on('message', async message => {
-    if (!message.guild) return;
-    if (message.content === '/pogrzeb') {
-        console.log(`pogrzeb sie zaczal`);
-      if (message.member.voice.channel) {
 
-message.member.voice.channel.join().then(VoiceConnection => {
-        VoiceConnection.play("pogrzeb.mp3").on("finish", () => VoiceConnection.disconnect());
-        message.reply("odprawianie...");
-    }).catch(e => console.log(e))
-} else {
-        message.reply('Nie ma cie na voice chacie');
-      }
-    }
-
-
-
-    if (message.content === '/stop') {
-
-      for (const connection of client.voice.connections.values()) {
-        connection.disconnect();
-    }
-      console.log(`pogrzeb sie zakonczyl`);     
-      }
-
-if (message.content === '/pomoc') {
-
-        message.reply("/pogrzeb - odprawia pogrzeb");
-	message.reply("/stop - konczy pogrzeb");
-  message.reply("/pingu - ping sprawdza w ms");
-	message.reply("https://cdn.discordapp.com/avatars/790380203296948224/96b25cdbb57b54fb52befca60fe3edfb.webp?size=1024");
-
-
-      console.log(`ktos pomocy szuka`);  
-
-}
-
-if (message.content === '/pingu') {
-    
-  var ping = Date.now() - message.createdTimestamp + " ms";
-  message.reply("Twoj ping to `" + `${Date.now() - message.createdTimestamp}` + " ms`");
-
-}
-
-  });
-
-client.login('TOKEN'); // your token
+client.login(config.token);
